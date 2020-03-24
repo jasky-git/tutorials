6f598e39c2971ce52b367b83a17ca2c74eb665a9
jasky.ong.2017 <jasky.ong.2017@sis.smu.edu.sg>
2020-03-24 20:18:14 +0800

    added features.html, Introducing of Class Inheritance, Functionality of Arrow Functions and Variables

--- react/reactdemoapp/src/index.js
+++ react/reactdemoapp/src/index.js
@@ -10,7 +10,34 @@ require("@babel/core").transform("code", {
 });
 */
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
+
+const myfirstelement = <h1>Hello React!</h1>
+
+class Car {
+  constructor(name) {
+    this.brand = name;
+  }
+
+  present() {
+    return 'I have a ' + this.brand;
+  }
+}
+
+class Model extends Car {
+  constructor(name, mod) {
+    super(name);
+    this.model = mod;
+  }
+  show() {
+    return this.present() + ', it is a ' + this.model
+  }
+}
+
+const mycars = new Model("Ford", "Mustang");
+const mysecondelement = mycars.show();
+
+ReactDOM.render(mysecondelement, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
